c8c90cc4f07ebecbc6e645795c95a6a1ab55630e
John <johnbaironlr@gmail.com>
2019-06-12 18:17:00 -0500

    Models created

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  content          :text
+#  commentable_type :string
+#  commentable_id   :integer
+#  user_id          :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Comment < ApplicationRecord
+  belongs_to :commentable, polymorphic: true
+  belongs_to :user
+end
---
+++ app/models/question.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Question < ApplicationRecord
+  belongs_to :user
+  has_many :answers
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
+end
